eefe59ab271a9779f0c8420ed5e2c702115c4431
gi rocha <133895231+gymratgi0@users.noreply.github.com>
2023-10-18 08:00:18 -0300

    Delete PrjGame directory

--- PrjGame/src/main/java/com/senai/giovannarocha/PrjGame/repositories/JogoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.senai.giovannarocha.PrjGame.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.senai.giovannarocha.PrjGame.entities.Jogo;
-
-public interface JogoRepository extends JpaRepository<Jogo, Long>{
-
-}
